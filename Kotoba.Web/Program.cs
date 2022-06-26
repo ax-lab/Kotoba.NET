c2f930a5b7079bb2462ec613192eddfe56e545d7
ax-lab <98651633+ax-lab@users.noreply.github.com>
2022-06-26 20:53:13 -0300

    config: set up static file serving in web project
    - add static files directory in `App/public`
    - add favicon and basic resources
    - add file server and fallback to `index.html`

--- Kotoba.Web/Program.cs
+++ Kotoba.Web/Program.cs
@@ -1,12 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.FileProviders;
+
+const int PORT_SSL = 29801;
+const int PORT_HTTP = 29802;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+	WebRootPath = "App/public",
+});
 
 builder.WebHost.UseUrls(
-	"https://0.0.0.0:29802",
-	"http://0.0.0.0:29801"
+	String.Format("https://0.0.0.0:{0}", PORT_SSL),
+	String.Format("http://0.0.0.0:{0}", PORT_HTTP)
 );
 
 var app = builder.Build();
+app.Lifetime.ApplicationStarted.Register(() =>
+{
+	app.Logger.LogInformation("Local listener at https://localhost:{0}", PORT_SSL);
+	app.Logger.LogInformation("Local listener at http://localhost:{0}", PORT_HTTP);
+});
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello", () => "hello world!!!");
+app.UseFileServer();
+
+app.MapFallbackToFile("/index.html");
 
 app.Run();
